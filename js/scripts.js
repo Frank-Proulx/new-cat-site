a65cbb5763dd0768547a8f9db7a7cd532303e89b
Frank-Proulx <frankproulx7@gmail.com>
2021-10-19 14:05:50 -0700

    add light/dark mode button

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  const buttonMode = document.getElementById("button-mode");
+
+
+  $("button").click(function() {
+    $("body").toggleClass("dark");
+    if (buttonMode.textContent === "Click here for Dark Mode") {
+      buttonMode.textContent = "Click here for Light Mode";
+    } else {
+      buttonMode.textContent = "Click here for Dark Mode";
+    }
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+});
